8c8d542e7b09f6daa9eb0a2551bbaea8b72aa9de
Lucas Arch <luketsx@icloud.com>
2026-01-26 01:45:02 -0300

    feat: refactor user management API; implement CRUD operations and validation with Zod

---
+++ examples/2-using-providers/src/app/routes/users/[userId]/route.delete.ts
@@ -0,0 +1,19 @@
+import { FindUserParams, FindUserParamsSchema } from "@/lib/users/schema";
+import { UserService } from "@/lib/users/service";
+import { inject, LithiaHandler, LithiaMiddleware, useParams, validate } from "@lithiajs/core";
+
+export const middlewares: LithiaMiddleware[] = [
+  validate({
+    params: FindUserParamsSchema
+  })
+]
+
+const handler: LithiaHandler = async (_, res) => {
+  const params = useParams<FindUserParams>();
+  const userService = inject(UserService);
+  await userService.deleteUser(params.userId);
+  
+  res.status(204).send();
+}
+
+export default handler;
